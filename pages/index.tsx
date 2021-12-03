ba5298d097384d7d6efb09d39175733dd4cb60a6
maggie-j-liu <maggie.j.liu@gmail.com>
2021-12-03 09:58:14 -0800

    display public links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
+import prisma from "@/lib/prisma";
+import { Link as LinkType } from "@prisma/client";
+import { GetServerSideProps } from "next";
 import Head from "next/head";
+import Link from "next/link";
 
-export default function Home() {
+export default function Home({ links }: { links: LinkType[] }) {
   return (
     <div>
       <Head>
@@ -10,8 +14,8 @@ export default function Home() {
           content="A link shortener for Leland's Computer Science Club."
         />
       </Head>
-      <main className={"flex items-center justify-center w-screen h-screen"}>
-        <p className="text-2xl">
+      <main className={"flex flex-col items-center py-16"}>
+        <p className="text-2xl mb-4">
           Link shortener for{" "}
           <a
             className="text-blue-600 font-semibold underline"
@@ -20,7 +24,47 @@ export default function Home() {
             Leland CS Club
           </a>
         </p>
+        <div className="px-8">
+          <div className="max-w-2xl w-full flex flex-col divide-y-2">
+            <div className="py-2 flex items-center text-center text-sm uppercase bg-gray-100 rounded-md">
+              <div className="w-1/3">Route</div>
+              <div className="w-2/3">URL</div>
+            </div>
+            {links.map((link) => (
+              <div key={link.slug} className="py-2 flex items-center">
+                <div className="w-1/3">
+                  <Link href={link.slug}>
+                    <a className="font-medium text-blue-500">{link.slug}</a>
+                  </Link>
+                </div>
+                <div className="w-2/3 break-all">
+                  <a
+                    className="text-blue-500"
+                    href={link.url}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {link.url}
+                  </a>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </main>
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const links = await prisma.link.findMany({
+    where: {
+      public: true,
+    },
+  });
+  return {
+    props: {
+      links,
+    },
+  };
+};
